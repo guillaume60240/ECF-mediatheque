3128304a9085a3f2991d2dee15701c37441fcd86
guillaume60240 <77913074+guillaume60240@users.noreply.github.com>
2021-10-05 11:22:44 +0200

    create animations for the li, h2 and btn. Only mobile style ok, see for responsive later

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,20 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+//animation page d'accueil
+const titleSpans = document.querySelectorAll('h2 span');
+const btns = document.querySelectorAll('button');
+const ul = document.querySelectorAll('.category');
+
+
+window.addEventListener('load', () => {
+
+    const TL = gsap.timeline({paused: true});
+
+    TL.staggerFrom(titleSpans, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
+    TL.staggerFrom(btns, 1, {opacity: 0, ease: "power2.out"}, 0.3, '-=1.5')
+    TL.staggerFrom(ul, 1, {opacity: 0, ease: "power2.out"}, 0.3, '-=1.5');
+
+    TL.play();
+})
