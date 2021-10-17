2a5af02cbacb65e3412e8585fb3954b642bf89d1
guillaume60240 <77913074+guillaume60240@users.noreply.github.com>
2021-10-17 15:52:16 +0200

    search book feature ok

--- src/Controller/ShowOneBookController.php
+++ src/Controller/ShowOneBookController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,8 +18,24 @@ class ShowOneBookController extends AbstractController
         $book = $bookRepository->findOneBy(['titleSlug' => $bookTitleSlug]);
 
         return $this->render('show_books/one.html.twig', [
-            'controller_name' => 'ShowOneBookController',
             'book' => $book
         ]);
+    }
+
+    /**
+     * @Route("/membre/recherche-par-nom", name="show_one_book_by_name")
+     */
+    public function bookByName(BookRepository $bookRepository,Request $request): Response
+    {
+        $title = $request->get('booksChoices');
+        if($title){
+            $book = $bookRepository->findOneBy(['title' => $title]);
+            
+            return $this->render('show_books/one.html.twig', [
+                'book' => $book
+            ]);
+        } else {
+            return $this->redirectToRoute('show_books');
+        }
     }
 }
