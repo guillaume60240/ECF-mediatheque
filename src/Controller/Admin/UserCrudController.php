2caa0fa20603dd9eccb52a2d15c0dd4b9fc0a22a
guillaume60240 <77913074+guillaume60240@users.noreply.github.com>
2021-10-17 11:56:27 +0200

    add view for userRoles

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -8,6 +8,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -17,6 +18,7 @@ class UserCrudController extends AbstractCrudController
 {
     protected $entityManager;
     protected $adminUrlGenerator;
+    
 
     public function __construct(EntityManagerInterface $entityManager, AdminUrlGenerator $adminUrlGenerator)
     {
@@ -31,25 +33,41 @@ class UserCrudController extends AbstractCrudController
 
     public function configureActions(Actions $actions): Actions
     {
-        $accountValidation = Action::new('accountValidation', 'Valider le compte')->linkToCrudAction('accountValidation');
-        $giveRole = Action::new('giveRole', 'Assigner comme employé')->linkToCrudAction('giveRole');
-
+        $accountValidation = Action::new('accountValidation', 'Valider le compte')
+                                ->linkToCrudAction('accountValidation')
+                                ->setIcon('fa fa-user-check');
+        $accountRestriction = Action::new('accountRestriction', 'Invalider le compte')
+                                ->linkToCrudAction('accountRestriction')
+                                ->addCssClass('btn btn-danger')
+                                ->setIcon('fas fa-user-slash');
+        $giveRole = Action::new('giveRole', 'Assigner comme employé')
+                                ->linkToCrudAction('giveRole')
+                                ->setIcon('fas fa-users-cog');
+        $deleteRole = Action::new('deleteRole', 'Supprimer employé')
+                                ->linkToCrudAction('deleteRole')
+                                ->addCssClass('btn btn-danger')
+                                ->setIcon('fas fa-users-slash');
         if($this->container->get('security.authorization_checker')->isGranted('ROLE_SUPER_ADMIN')){
             return $actions
             ->add('detail', $accountValidation)
+            ->add('detail', $accountRestriction)
             ->add('detail', $giveRole)
-            ->add('index', 'detail');
+            ->add('detail', $deleteRole)
+            ->add('index', 'detail')
+            ->disable(Action::NEW, Action::DELETE, Action::EDIT);
         } else{
             return $actions
             ->add('detail', $accountValidation)
-            ->add('index', 'detail');
+            ->add('detail', $accountRestriction)
+            ->add('index', 'detail')
+            ->disable(Action::NEW, Action::DELETE, Action::EDIT);
         }
     }
 
     public function accountValidation(AdminContext $context)
     {
         $action = $context->getEntity()->getInstance();
-        if($action->getAccountValidate() === false){
+        if($action->getAccountValidate() === false && !in_array('ROLE_SUPER_ADMIN', $action->getRoles()) && !in_array('ROLE_ADMIN', $action->getRoles())){
 
             $action->setAccountValidate(true);
             $action->setRoles(['ROLE_MEMBER']);
@@ -64,11 +82,28 @@ class UserCrudController extends AbstractCrudController
         return $this->redirection($action);
     }
 
+    public function accountRestriction(AdminContext $context)
+    {
+        $action = $context->getEntity()->getInstance();
+        if($action->getAccountValidate() === true && !in_array('ROLE_SUPER_ADMIN', $action->getRoles()) && !in_array('ROLE_ADMIN', $action->getRoles())){
+            $action->setAccountValidate(false);
+            $action->setRoles([]);
+            $this->entityManager->flush();
+            
+            $this->addFlash('notice', "<span style='color:green;'>Le compte a été invalidé</span>");
+
+        }else{
+            $this->addFlash('notice', "<span style='color:red;'>Vous ne pouvez pas effectuer cette action.</span>");
+        }
+
+        return $this->redirection($action);
+    }
+
     public function giveRole(AdminContext $context)
     {
         $action = $context->getEntity()->getInstance();
 
-        if (in_array('ROLE_ADMIN', $action->getRoles())){
+        if (in_array('ROLE_ADMIN', $action->getRoles()) || in_array('ROLE_SUPER_ADMIN', $action->getRoles())){
             
             $this->addFlash('notice', "<span style='color:red;'>Vous ne pouvez pas effectuer cette action.</span>");
 
@@ -77,6 +112,24 @@ class UserCrudController extends AbstractCrudController
             $this->entityManager->flush();
             
             $this->addFlash('notice', "<span style='color:green;'>Ce membre a le rôle employé.</span>");
+        }
+
+        return $this->redirection($action);
+    }
+
+    public function deleteRole(AdminContext $context)
+    {
+        $action = $context->getEntity()->getInstance();
+
+        if (!in_array('ROLE_ADMIN', $action->getRoles()) || in_array('ROLE_SUPER_ADMIN', $action->getRoles())){
+            
+            $this->addFlash('notice', "<span style='color:red;'>Vous ne pouvez pas effectuer cette action.</span>");
+
+        } else{
+            $action->setRoles(['ROLE_MEMBER']);
+            $this->entityManager->flush();
+            
+            $this->addFlash('notice', "<span style='color:green;'>Cet utilisateur est devenu simple membre.</span>");
         }
 
         return $this->redirection($action);
@@ -92,7 +145,7 @@ class UserCrudController extends AbstractCrudController
             
         return $this->redirect($url);
     }
-    
+
     public function configureFields(string $pageName): iterable
     {
         return [
@@ -107,7 +160,10 @@ class UserCrudController extends AbstractCrudController
                 ->setFormTypeOption('disabled','disabled'),
             BooleanField::new('accountValidate', 'Compte validé')
                 ->setFormTypeOption('disabled','disabled'),
-            AssociationField::new('reservations', 'Nombre de réservations')
+            AssociationField::new('reservations', 'Nombre de réservations'),
+            ArrayField::new('roles')
+                ->setLabel('Role')
+                ->setFormTypeOption('disabled', 'disabled'),
         ];
     }
     
