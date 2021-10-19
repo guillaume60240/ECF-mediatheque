7ffaf308bb8460e289092d3f8c0723ed5055a97d
guillaume60240 <77913074+guillaume60240@users.noreply.github.com>
2021-10-19 19:11:23 +0200

    change link for mail validation

--- src/Services/Mail/MailService.php
+++ src/Services/Mail/MailService.php
@@ -18,7 +18,7 @@ class MailService
     {
         $mail = new Mail;
         $code = random_int(1000, 9999);
-        $link = 'Lien : <a href="http://127.0.0.1:8000/valider-mon-mail">Suivre ce lien</a>';
+        $link = 'Lien : <a href="https://ecfmediatheque.fr/valider-mon-mail">Suivre ce lien</a>';
         $name = $user->getName().' '.$user->getFirstname();
 
         $mail->sendValidation($user->getEmail(), $user->getName().' '.$user->getFirstname(), 'Validation de mail',$name, $code, $link, 3262622);
